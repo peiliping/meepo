1cfdf19b40df07863202d3202d0b87d4388c8ebe
peiliping <peilipingplp@gmail.com>
2017-04-14 10:21:22 +0800

    support upload avsc to hdfs

--- src/main/java/meepo/transform/sink/parquet/ParquetSinkHelper.java
+++ src/main/java/meepo/transform/sink/parquet/ParquetSinkHelper.java
@@ -1,9 +1,7 @@
 package meepo.transform.sink.parquet;
 
-import org.apache.avro.Schema;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.avro.AvroSchemaConverter;
 import org.apache.parquet.hadoop.ParquetFileWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
@@ -20,19 +18,9 @@ public class ParquetSinkHelper extends ParquetWriter<Object[]> {
         super(file, new ParquetSinkSupport(schema), CompressionCodecName.SNAPPY, ParquetWriter.DEFAULT_BLOCK_SIZE / 4, ParquetWriter.DEFAULT_PAGE_SIZE);
     }
 
-    @SuppressWarnings("deprecation") public ParquetSinkHelper(Path file, MessageType schema, String classpath) throws IOException {
+    @SuppressWarnings("deprecation") public ParquetSinkHelper(Path file, MessageType schema, Configuration conf) throws IOException {
         super(file, ParquetFileWriter.Mode.CREATE, new ParquetSinkSupport(schema), CompressionCodecName.SNAPPY, ParquetWriter.DEFAULT_BLOCK_SIZE / 4,
-                ParquetWriter.DEFAULT_PAGE_SIZE, ParquetWriter.DEFAULT_PAGE_SIZE, DEFAULT_IS_DICTIONARY_ENABLED, DEFAULT_IS_VALIDATING_ENABLED, DEFAULT_WRITER_VERSION,
-                createConf(classpath));
-        Schema sca = (new AvroSchemaConverter()).convert(schema);
-        //TODO write schema to hdfs
-    }
-
-    public static Configuration createConf(String classpath) {
-        Configuration conf = new Configuration();
-        conf.addResource(new Path(classpath + "core-site.xml"));
-        conf.addResource(new Path(classpath + "hdfs-site.xml"));
-        return conf;
+                ParquetWriter.DEFAULT_PAGE_SIZE, ParquetWriter.DEFAULT_PAGE_SIZE, DEFAULT_IS_DICTIONARY_ENABLED, DEFAULT_IS_VALIDATING_ENABLED, DEFAULT_WRITER_VERSION, conf);
     }
 
 }
