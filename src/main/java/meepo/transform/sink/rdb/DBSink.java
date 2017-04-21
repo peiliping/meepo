a61ab411a507c68b95e62474a76d6a222b07c263
peiliping <peilipingplp@gmail.com>
2017-04-21 13:43:36 +0800

    u

--- src/main/java/meepo/transform/sink/rdb/DBSink.java
+++ src/main/java/meepo/transform/sink/rdb/DBSink.java
@@ -145,7 +145,7 @@ public class DBSink extends AbstractSink {
                     c.setAutoCommit(false);
                     p = c.prepareStatement(sql);
                     StatementImpl target = (StatementImpl) ((DruidPooledPreparedStatement) p).getStatement();
-                    batchArgsField.set(target, new ArrayList<Object>(stepSize + 10));
+                    batchArgsField.set(target, new ArrayList<Object>(stepSize));
                 }
             } catch (Exception e) {
                 LOG.error("DBSink-Handler-Prepare Error :", e);
