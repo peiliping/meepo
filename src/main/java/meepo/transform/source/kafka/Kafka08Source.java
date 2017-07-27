0d040a5ad187f7795733adf6f251197d49739f1c
peiliping <peilipingplp@gmail.com>
2017-07-27 13:56:43 +0800

    完善kafka作为source的基类

--- src/main/java/meepo/transform/source/kafka/Kafka08Source.java
+++ src/main/java/meepo/transform/source/kafka/Kafka08Source.java
@@ -1,12 +1,19 @@
 package meepo.transform.source.kafka;
 
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.ConsumerTimeoutException;
+import kafka.consumer.*;
 import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.message.MessageAndMetadata;
+import kafka.serializer.DefaultDecoder;
+import kafka.serializer.StringDecoder;
+import kafka.utils.VerifiableProperties;
 import meepo.transform.channel.RingbufferChannel;
 import meepo.transform.config.TaskContext;
 import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by peiliping on 17-7-27.
@@ -19,6 +26,15 @@ public class Kafka08Source extends KafkaSource {
 
     public Kafka08Source(String name, int index, int totalNum, TaskContext context, RingbufferChannel rb) {
         super(name, index, totalNum, context, rb);
+        ConsumerConfig consumerConfig = new ConsumerConfig(toProps());
+        this.consumer = Consumer.createJavaConsumerConnector(consumerConfig);
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(context.get("topic"), 1);
+        Map<String, List<KafkaStream<String, byte[]>>> consumerMap =
+                consumer.createMessageStreams(topicCountMap, new StringDecoder(new VerifiableProperties()), new DefaultDecoder(new VerifiableProperties()));
+        List<KafkaStream<String, byte[]>> topicList = consumerMap.get(context.get("topic"));
+        KafkaStream<String, byte[]> stream = topicList.get(0);
+        this.consumerIterator = stream.iterator();
     }
 
     @Override
@@ -36,5 +52,23 @@ public class Kafka08Source extends KafkaSource {
         } catch (ConsumerTimeoutException e) {
             return false;
         }
+    }
+
+    @Override
+    public Properties toProps() {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", super.kafkaContext.get("zookeeper"));
+        props.put("group.id", super.kafkaContext.get("group"));
+        props.put("auto.commit.enable", "true");
+        props.put("auto.commit.interval.ms", super.kafkaContext.getString("commitInterval", "10000"));
+        props.put("consumer.timeout.ms", "3000");
+        props.put("fetch.message.max.bytes", super.kafkaContext.get("msgSize"));
+        return props;
+    }
+
+    @Override
+    public void end() {
+        super.end();
+        this.consumer.shutdown();
     }
 }
--- src/main/java/meepo/transform/source/kafka/serialize/IKafkaSerialize.java
+++ src/main/java/meepo/transform/source/kafka/serialize/IKafkaSerialize.java
@@ -1,11 +1,16 @@
 package meepo.transform.source.kafka.serialize;
 
 import meepo.transform.channel.DataEvent;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
 
 /**
  * Created by peiliping on 17-7-27.
  */
 public interface IKafkaSerialize {
+
+    public void initSchema(List<Pair<String, Integer>> schema);
 
     public void parse(byte[] msg, DataEvent de);
 
